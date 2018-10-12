8c6eb674b4e9f154a3ee58576fc6142b7f698bb5
ramireddy99 <harshavardhanreddy.r@thirdware.com>
2018-10-12 16:35:11 +0530

    TaskThree

--- TaskThree/RemovePalindrome.java
+++ TaskThree/RemovePalindrome.java
@@ -12,17 +12,22 @@ public class RemovePalindrome {
 		for (int i = 0; i < sp.length; i++) {
 			String st1 = sp[i];
 
-			StringBuilder input1 = new StringBuilder();
+			if(st1.length()>1)
+			{
+				StringBuilder input1 = new StringBuilder();
 
-			input1.append(st1);
+				input1.append(st1);
 
-			input1 = input1.reverse();
-			String st2 = input1.toString();
+				input1 = input1.reverse();
+				String st2 = input1.toString();
 
-			if (st1.equals(st2)) {
-				System.out.print(" ");
-			} else {
-				System.out.print(st1 + "  ");
+				if (st1.equals(st2)) {
+					System.out.print(" ");
+				} else {
+					System.out.print(st1 + "  ");
+				}
+			}else{
+			System.out.print(st1+" ");
 			}
 		}
 		scanner.close();
